42879e6ea6cc3ae6c625fa646adac6a212f0b06a
Ioanna Mourtzaki <109597293+mourtzakiiwanna@users.noreply.github.com>
2023-09-09 18:40:22 +0300

    commit

--- src/Prototype.js
+++ src/Prototype.js
@@ -23,8 +23,68 @@ function Prototype() {
     constraint: '',
     defaultValue: '',
   });
-  const [addInheritance, setAddInheritance] = useState(false);
-  const [newInheritance, setNewInheritance] = useState('');
+
+  const [dropdownOptions, setDropdownOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    // Fetch dropdown options from the URL when the component mounts
+    fetch("http://localhost:8080/getLocal")
+      .then((response) => response.json())
+      .then((data) => setDropdownOptions(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const handleDropdownChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedOption(selectedValue);
+  };
+
+  const handleAddInheritanceClick = () => {
+    setShowDropdown(true);
+  };
+
+  const handleSaveInheritance = () => {
+    // Handle saving the selected inheritance here
+    // You can use the value in 'selectedOption'
+    // For example, send it to your server or update state
+    setShowDropdown(false); // Hide the dropdown after saving
+  };
+
+  const handleCancel = () => {
+    setShowDropdown(false);
+  };
+
+  // const [addInheritance, setAddInheritance] = useState(false);
+  // const [newInheritance, setNewInheritance] = useState('');
+  // const [dropdownOptions, setDropdownOptions] = useState([]);
+  // const [showDropdown, setShowDropdown] = useState(false);
+
+  // const [selectedOption, setSelectedOption] = useState("");
+
+  // useEffect(() => {
+  //   // Fetch dropdown options from the URL when the component mounts
+  //   fetch("http://localhost:8080/getLocal")
+  //     .then((response) => response.json())
+  //     .then((data) => setDropdownOptions(data))
+  //     .catch((error) => console.error("Error fetching data:", error));
+  // }, []);
+
+  // const handleDropdownChange = (e) => {
+  //   const selectedValue = e.target.value;
+  //   setSelectedOption(selectedValue);
+  // };
+
+
+    // const handleInputChange = (e) => {
+    //   const value = e.target.value;
+    //   setNewInheritance(value);
+
+    //   // Show the dropdown when the input field is not empty
+    //   setShowDropdown(value !== "");
+    // };
+
 
   const fetchData = async () => {
     try {
@@ -77,52 +137,52 @@ function Prototype() {
 
 
 
-  const handleAddInheritanceClick = () => {
-    setAddInheritance(true);
-  };
+  // const handleAddInheritanceClick = () => {
+  //   setAddInheritance(true); // Set addInheritance to true when the button is clicked
+  // };
 
-  const handleSaveInheritance = async () => {
-    try {
-      var fullPath = "";
-      if (groupName == "core") {
-        fullPath = "butterfly" + "/" + groupName + "/" + prototypeName;
-      } else {
-        fullPath = groupName + "/" + prototypeName;
-      }      
+  // const handleSaveInheritance = async () => {
+  //   try {
+  //     var fullPath = "";
+  //     if (groupName == "core") {
+  //       fullPath = "butterfly" + "/" + groupName + "/" + prototypeName;
+  //     } else {
+  //       fullPath = groupName + "/" + prototypeName;
+  //     }      
       
-      const response = await fetch(`http://localhost:8080/updateInheritance?prototypePath=/${fullPath}&inheritedPrototypes=${newInheritance}`);
+  //     const response = await fetch(`http://localhost:8080/updateInheritance?prototypePath=/${fullPath}&inheritedPrototypes=${newInheritance}`);
 
-      if (!response.ok) {
-        console.error('Error updating inheritance');
-        return;
-      }
+  //     if (!response.ok) {
+  //       console.error('Error updating inheritance');
+  //       return;
+  //     }
   
-      let responseData;
+  //     let responseData;
 
-      const cloneResponse = response.clone(); // Clone the response object
+  //     const cloneResponse = response.clone(); // Clone the response object
 
-      try {
-        responseData = await cloneResponse.json(); // Attempt to parse JSON data
-      } catch (error) {
-        responseData = await response.text(); // Get the plain text response
-      }
+  //     try {
+  //       responseData = await cloneResponse.json(); // Attempt to parse JSON data
+  //     } catch (error) {
+  //       responseData = await response.text(); // Get the plain text response
+  //     }
 
-      if (responseData == "Updated") {
-        alert("Prototype updated successfully!")
-      } else {
-        alert("This prototype does not exist or they are already inherited.")
-      }
+  //     if (responseData == "Updated") {
+  //       alert("Prototype updated successfully!")
+  //     } else {
+  //       alert("This prototype does not exist or they are already inherited.")
+  //     }
 
      
 
-      setAddInheritance(false);
-      setNewInheritance('');
-      fetchData();
+  //     setAddInheritance(false);
+  //     setNewInheritance('');
+  //     fetchData();
       
-    } catch (error) {
-      console.error('API call error:', error);
-    }
-  };
+  //   } catch (error) {
+  //     console.error('API call error:', error);
+  //   }
+  // };
 
   useEffect(() => {
     fetchData();
@@ -132,150 +192,140 @@ function Prototype() {
     return <Link to={"/"}></Link>;
   }
 
+
+
   return (
-    
     <div>
       <SideMenu />
       <div className="main-content">
+        <div className="prototype-container">
+          <button onClick={() => navigation("/")} className="back-button">Back</button> 
+          <h2 className="header">{prototypeInfo.id.replace(/([A-Z])/g, ' $1')}</h2>
+          <div className="info-section">
+            <h3 className="subsubheader">Prototype information</h3>
+            <p className="info-prototypes">
+              <p><span>ID:</span> {prototypeInfo.id}</p>
+              <p><span>Abstract:</span> {prototypeInfo.isAbstract.toString()}</p>
+              <p><span>Number of Fields:</span> {prototypeInfo.numOfFields}</p>
+            </p>
+          </div>
+  
 
-      <div className="prototype-container">
-
-        <button onClick={() => navigation("/")}  className="back-button">Back</button> 
-        <h2 className="header">{prototypeInfo.id.replace(/([A-Z])/g, ' $1')}</h2>
-
-        <div className="info-section">
-          <h3 className="subsubheader">Prototype information</h3>
-          <p className="info-prototypes">
-            <p><span>ID:</span> {prototypeInfo.id}</p>
-            <p><span>Abstract:</span> {prototypeInfo.isAbstract.toString()}</p>
-            <p><span>Number of Fields:</span> {prototypeInfo.numOfFields}</p>
-          </p>
-        </div>
-
-        <div className="inheritance-section">
-          <h3 className="subsubheader">Inheritance</h3>
-
-        
-
-          {prototypeInfo.inheritedPrototypes.length > 0 && (
+          <div className="inheritance-section">
+            <h3 className="subsubheader">Inheritance</h3>
             <p className="inherited-prototypes">
               <span>Inherited Prototypes:</span> {prototypeInfo.inheritedPrototypes.join(', ')}
             </p>
-          )}
-
-          {prototypeInfo.allInheritedPrototypes.length > 0 && (
             <p className="all-inherited-prototypes">
               <span>All Inherited Prototypes:</span> {prototypeInfo.allInheritedPrototypes.join(', ')}
             </p>
-          )}
+            {showDropdown ? (
+              <div className="add-inheritance-input">
+                <select
+                  id="inheritedPrototype"
+                  className="select-dropdown"
+                  value={selectedOption}
+                  onChange={handleDropdownChange}
+                >
+              <option value="" disabled >Select an inherited prototype</option>                  
+              {dropdownOptions.map((option) => (
+                    <option key={option} value={option} className="option">
+                      {option}
+                    </option>
+                  ))}
+                </select>
+                <button className="save-button" onClick={handleSaveInheritance}>Save</button>
+                <button className="cancel-button" onClick={handleCancel}>Cancel</button>
 
-        {addInheritance ? (
-                  <div className="add-inheritance-input">
-                    <input
-                      type="text"
-                      value={newInheritance}
-                      onChange={(e) => setNewInheritance(e.target.value)}
-                      placeholder="Enter prototype model"
-                    />
-                    <button onClick={handleSaveInheritance}>Save</button>
-                  </div>
-                ) : (
-                  <button className="add-inheritance-button" onClick={handleAddInheritanceClick}>
-                    Add Inherited Prototype
-                  </button>
-        )}
-
+              </div>
+            ) : (
+              <button className="add-button" onClick={handleAddInheritanceClick}>
+                Add Inherited Prototype
+              </button>
+            )}
         </div>
-
         
-
-        <div className="subheader">Fields</div>
-        <ul className="fields-list">
-          {prototypeInfo.fields.map((field, index) => (
-            <li key={index} className="field-item">
-              <p><span>Field ID:</span> {field.id}</p>
-              <p><span>Field Type:</span> {field.attributeType}</p>
-              {field.constraints && (
-                <p><span>Constraints:</span> {field.constraints}</p>
-              )}
-              {field.alias && (
-                <p><span>Alias:</span> {field.alias}</p>
-              )}
-            </li>
-          ))}
-        </ul>
-        {!isFormVisible && (
-          <button className="add-field-button" onClick={handleToggleForm}>
-            Add Field
-          </button>
-        )}
-        {isFormVisible && (
-          <div className="add-field-form">
-            <h3>Add New Field</h3>
-            <div className="field-input">
-            <label>ID</label>
-            <input
-              type="text"
-              value={newFieldInfo.id}
-              onChange={(e) =>
-                setNewFieldInfo({ ...newFieldInfo, id: e.target.value })
-              }
-            />
-          </div>
-          <div className="field-input">
-            <label>Field Group ID</label>
-            <input
-              type="text"
-              value={newFieldInfo.fgId}
-              onChange={(e) =>
-                setNewFieldInfo({ ...newFieldInfo, fgId: e.target.value })
-              }
-            />
-          </div>
-     
-           
-        
-
-       
-          <div className="field-input">
-            <label>Constraint</label>
-            <input
-              type="text"
-              value={newFieldInfo.constraint}
-              onChange={(e) =>
-                setNewFieldInfo({ ...newFieldInfo, constraint: e.target.value })
-              }
-            />
-          </div>
-          <div className="field-input">
-            <label>Default Value</label>
-            <input
-              type="text"
-              value={newFieldInfo.defaultValue}
-              onChange={(e) =>
-                setNewFieldInfo({ ...newFieldInfo, defaultValue: e.target.value })
-              }
-            />
-          </div>   
-
-          <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Value Type" />
-          <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Trans" />
-          <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Array" />
-          <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Map" />
-
-              
-          <button className="save-field-button" onClick={handleAddField}>
-              Save
+          <div className="subheader">Fields</div>
+          <ul className="fields-list">
+            {prototypeInfo.fields.map((field, index) => (
+              <li key={index} className="field-item">
+                <p><span>Field ID:</span> {field.id}</p>
+                <p><span>Field Type:</span> {field.attributeType}</p>
+                {field.constraints && (
+                  <p><span>Constraints:</span> {field.constraints}</p>
+                )}
+                {field.alias && (
+                  <p><span>Alias:</span> {field.alias}</p>
+                )}
+              </li>
+            ))}
+          </ul>
+          {!isFormVisible && (
+            <button className="add-field-button" onClick={handleToggleForm}>
+              Add Field
             </button>
-            <button className="cancel-field-button" onClick={handleToggleForm}>
-              Cancel
-            </button>
-          </div>
-        )}
+          )}
+          {isFormVisible && (
+            <div className="add-field-form">
+              <h3>Add New Field</h3>
+              <div className="field-input">
+                <label>ID</label>
+                <input
+                  type="text"
+                  value={newFieldInfo.id}
+                  onChange={(e) =>
+                    setNewFieldInfo({ ...newFieldInfo, id: e.target.value })
+                  }
+                />
+              </div>
+              <div className="field-input">
+                <label>Field Group ID</label>
+                <input
+                  type="text"
+                  value={newFieldInfo.fgId}
+                  onChange={(e) =>
+                    setNewFieldInfo({ ...newFieldInfo, fgId: e.target.value })
+                  }
+                />
+              </div>
+              <div className="field-input">
+                <label>Constraint</label>
+                <input
+                  type="text"
+                  value={newFieldInfo.constraint}
+                  onChange={(e) =>
+                    setNewFieldInfo({ ...newFieldInfo, constraint: e.target.value })
+                  }
+                />
+              </div>
+              <div className="field-input">
+                <label>Default Value</label>
+                <input
+                  type="text"
+                  value={newFieldInfo.defaultValue}
+                  onChange={(e) =>
+                    setNewFieldInfo({ ...newFieldInfo, defaultValue: e.target.value })
+                  }
+                />
+              </div>
+              <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Value Type" />
+              <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Trans" />
+              <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Array" />
+              <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 15 } }}/>} label="Map" />
+              <button className="save-field-button" onClick={handleAddField}>
+                Save
+              </button>
+              <button className="cancel-field-button" onClick={handleToggleForm}>
+                Cancel
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-    </div>
   );
+  
+
 }
 
 export default Prototype;
