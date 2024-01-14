d6eeb6fde03b076ddba381f895276d3700f5c405
Ioanna Mourtzaki <109597293+mourtzakiiwanna@users.noreply.github.com>
2024-01-14 13:25:14 +0200

    add prototype

--- src/AddPrototypePage.js
+++ src/AddPrototypePage.js
@@ -7,184 +7,183 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Snackbar from '@mui/material/Snackbar';
 import TextField from '@mui/material/TextField';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 
 function AddPrototypePage() {
 
-  const baseURL = 'http://localhost:8080/api/type';
+    const baseURL = 'http://localhost:8080/api/type';
 
-  const navigate = useNavigate();
-  const [alert, setAlert] = useState(false);
-  const [alertContent, setAlertContent] = useState('');
-  const [prototypeName, setPrototypeName] = useState('');
-  const [success, setSuccess] = useState(false);
+    const navigate = useNavigate();
+    const [alert, setAlert] = useState(false);
+    const [alertContent, setAlertContent] = useState('');
+    const [prototypeName, setPrototypeName] = useState('');
+    const [success, setSuccess] = useState(false);
 
-  let headers = new Headers();
+    let headers = new Headers();
 
-  headers.append('Content-Type', 'application/json');
-  headers.append('Accept', 'application/json');
-  headers.append('Origin', 'http://localhost:3000');
-  const handleNameChange = (event) => {
-    setPrototypeName(event.target.value);
-  };
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Origin', 'http://localhost:3000');
+    const handleNameChange = (event) => {
+        setPrototypeName(event.target.value);
+    };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    if (prototypeName) {
-      try {
-        const existingPrototypes = await fetchExistingPrototypes(`${baseURL}/category/local`);
+        if (prototypeName) {
+            try {
+                const existingPrototypes = await fetchExistingPrototypes(`${baseURL}/category/local`);
 
-        const isPrototypeExists = existingPrototypes.includes(prototypeName);
+                const isPrototypeExists = existingPrototypes.includes(prototypeName);
 
-        if (isPrototypeExists) {
-          setAlertContent('Prototype with the same name already exists. Please choose a different name.');
+                if (isPrototypeExists) {
+                    setAlertContent('Prototype with the same name already exists. Please choose a different name.');
+                } else {
+                    await createPrototype(`${baseURL}/${prototypeName}`);
+                    setSuccess(true);
+                    setAlertContent('The prototype has been successfully created!');
+                    setTimeout(() => {
+                        navigate(`/prototype/local/${prototypeName}`);
+                    }, 2000);
+                }
+            } catch (error) {
+                console.error('Error checking existing prototypes or creating prototype:', error);
+                setAlertContent('An error occurred. Please try again.');
+            }
         } else {
-          await createPrototype(`${baseURL}/${prototypeName}`);
-          setSuccess(true);
-          setAlertContent('The prototype has been successfully created!');
-          setTimeout(() => {
-            navigate(`/prototype/local/${prototypeName}`);
-          }, 2000);
+            setAlertContent('Please enter a valid prototype name.');
         }
-      } catch (error) {
-        console.error('Error checking existing prototypes or creating prototype:', error);
-        setAlertContent('An error occurred. Please try again.');
-      }
-    } else {
-      setAlertContent('Please enter a valid prototype name.');
+
+        setAlert(true);
+        setTimeout(() => {
+            setAlert(false);
+        }, 2000);
+    };
+
+    async function fetchExistingPrototypes(url = "") {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: headers,
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
     }
 
-    setAlert(true);
-    setTimeout(() => {
-      setAlert(false);
-    }, 2000);
-  };
+    async function createPrototype(url = "") {
+        const response = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: headers,
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+        });
 
-  async function fetchExistingPrototypes(url = "") {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: headers,
-    });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        return response.json();
     }
 
-    const data = await response.json();
-    return data;
-  }
+    return (
+        <div>
+            <Box
+                sx={{
+                    height: '100%',
+                    display: 'flex',
+                    marginTop: '240px',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Card sx={{width: '100%', maxWidth: '600px'}}>
+                    <CardContent sx={{margin: '70px'}}>
+                        <h3 className="add-prototype-title">Create Prototype</h3>
 
-  async function createPrototype(url = "") {
-    const response = await fetch(url, {
-      method: "POST",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: headers,
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-    });
+                        <form onSubmit={handleSubmit} noValidate={true}>
+                            <TextField
+                                required
+                                id="standard-required"
+                                label="Prototype Name"
+                                defaultValue=""
+                                variant="standard"
+                                type="text"
+                                size="small"
+                                fullWidth
+                                value={prototypeName}
+                                onChange={handleNameChange}
+                            />
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+                            <Box
+                                component="span"
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                marginTop="40px"
+                            >
+                                <Button
+                                    variant="text"
+                                    component={Link}
+                                    to="/"
+                                    sx={{
+                                        height: '40px',
+                                        color: '#9b9b9b',
+                                        textDecoration: 'underline',
+                                        '&:hover': {
+                                            color: '#535353',
+                                            textDecoration: 'underline',
+                                            backgroundColor: '#e6e6e6',
+                                        },
+                                    }}
+                                >
+                                    Cancel
+                                </Button>
 
-    return response.json();
-  }
+                                <Button
+                                    variant="contained"
+                                    type="submit"
+                                    size="small"
+                                    sx={{
+                                        backgroundColor: '#b3b3b3',
+                                        height: '40px',
+                                        color: '#fff',
+                                        border: 'none',
+                                        borderRadius: '5px',
+                                        minWidth: '80px',
+                                        marginLeft: '20px',
+                                        '&:hover': {
+                                            backgroundColor: '#9b9b9b',
+                                        },
+                                    }}
+                                >
+                                    Save
+                                </Button>
+                            </Box>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Box>
 
-
-  return (
-    <div>
-      <Box
-        sx={{
-          height: '100%',
-          display: 'flex',
-          marginTop: '240px',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Card sx={{ width: '100%', maxWidth: '600px' }}>
-          <CardContent sx={{ margin: '70px' }}>
-            <h3 className="add-prototype-title">Create Prototype</h3>
-
-            <form onSubmit={handleSubmit} noValidate={true}>
-              <TextField
-                required
-                id="standard-required"
-                label="Prototype Name"
-                defaultValue=""
-                variant="standard"
-                type="text"
-                size="small"
-                fullWidth
-                value={prototypeName}
-                onChange={handleNameChange}
-              />
-
-              <Box
-                component="span"
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                marginTop="40px"
-              >
-                <Button
-                  variant="text"
-                  component={Link}
-                  to="/"
-                  sx={{
-                    height: '40px',
-                    color: '#9b9b9b',
-                    textDecoration: 'underline',
-                    '&:hover': {
-                      color: '#535353',
-                      textDecoration: 'underline',
-                      backgroundColor: '#e6e6e6',
-                    },
-                  }}
+            <Snackbar open={alert} autoHideDuration={6000} onClose={() => setAlert(false)}>
+                <MuiAlert
+                    icon={success ? <CheckIcon/> : <WarningIcon/>}
+                    severity={success ? 'success' : 'error'}
+                    sx={{width: '100%', fontWeight: '500'}}
                 >
-                  Cancel
-                </Button>
-
-                <Button
-                  variant="contained"
-                  type="submit"
-                  size="small"
-                  sx={{
-                    backgroundColor: '#b3b3b3',
-                    height: '40px',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '5px',
-                    minWidth: '80px',
-                    marginLeft: '20px',
-                    '&:hover': {
-                      backgroundColor: '#9b9b9b',
-                    },
-                  }}
-                >
-                  Save
-                </Button>
-              </Box>
-            </form>
-          </CardContent>
-        </Card>
-      </Box>
-
-      <Snackbar open={alert} autoHideDuration={6000} onClose={() => setAlert(false)}>
-        <MuiAlert
-          icon={success ? <CheckIcon /> : <WarningIcon />}
-          severity={success ? 'success' : 'error'}
-          sx={{ width: '100%', fontWeight: '500' }}
-        >
-          {alertContent}
-        </MuiAlert>
-      </Snackbar>
-    </div>
-  );
+                    {alertContent}
+                </MuiAlert>
+            </Snackbar>
+        </div>
+    );
 }
 
 export default AddPrototypePage;
